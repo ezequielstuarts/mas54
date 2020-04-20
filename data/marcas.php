5327e0fddede46bedf684e7215602840767a4f27
Ezequiel <54673625+ezequielstuarts@users.noreply.github.com>
2020-04-20 14:03:13 -0300

    marcas

--- data/marcas.php
+++ data/marcas.php
@@ -2,87 +2,87 @@
 <?php 
 $array = [
     [
-    "logo" => "logo_bbva_color",
+    "logo" => "logo_bbva",
     "title" => "bbva",
     "url" => "assets/images/project/logo_bbva_color.jpg"
     ],
     [
-    "logo" => "logo_cofco_color",
+    "logo" => "logo_cofco",
     "title" => "cofco",
     "url" => "assets/images/project/logo_cofco_color.jpg"
     ],
     [
-    "logo" => "logo_poxipol_color",
+    "logo" => "logo_poxipol",
     "title" => "poxipol",
-    "url" => "assets/images/project/logo_poxipol_color.png"
+    "url" => "assets/images/project/logo_poxipol_chile.png"
     ],
     [
-    "logo" => "logo_gotita_color",
+    "logo" => "logo_gotita",
     "title" => "gotita",
     "url" => "assets/images/project/logo_gotita_color.png"
     ],
     [
-    "logo" => "logo_eccole_color",
+    "logo" => "logo_eccole",
     "title" => "eccole",
     "url" => "assets/images/project/logo_eccole_color.png"
     ],
     [
-    "logo" => "logo_figurella_color",
+    "logo" => "logo_figurella",
     "title" => "figurella",
     "url" => "assets/images/project/logo_figurella_color.jpg"
     ],
     [
-    "logo" => "logo_orizon_color",
+    "logo" => "logo_orizon",
     "title" => "orizon",
     "url" => "assets/images/project/logo_orizon_color.jpg"
     ],
     [
-    "logo" => "logo_cnv_color",
+    "logo" => "logo_cnv",
     "title" => "cnv",
     "url" => "assets/images/project/logo_cnv_color.jpg"
     ],
     [
-    "logo" => "logo_via_color",
+    "logo" => "logo_via",
     "title" => "via",
     "url" => "assets/images/project/logo_via_color.jpg"
     ],
     [
-    "logo" => "logo_aval_color",
+    "logo" => "logo_aval",
     "title" => "aval",
     "url" => "assets/images/project/logo_aval_color.jpg"
     ],
     [
-    "logo" => "logo_mb_color",
+    "logo" => "logo_mb",
     "title" => "mb",
     "url" => "assets/images/project/logo_mb_color.jpg"
     ],
     [
-    "logo" => "logo_prosegur_color",
+    "logo" => "logo_prosegur",
     "title" => "prosegur",
     "url" => "assets/images/project/logo_prosegur_color.jpg"
     ],
     [
-    "logo" => "logo_smart_color",
+    "logo" => "logo_smart",
     "title" => "smart",
     "url" => "assets/images/project/logo_smart_color.jpg"
     ],
     [
-    "logo" => "logo_tipem_color",
+    "logo" => "logo_tipem",
     "title" => "tipem",
     "url" => "assets/images/project/logo_tipem_color.jpg"
     ],
     [
-    "logo" => "logo_citaldoc_color",
+    "logo" => "logo_citaldoc",
     "title" => "citaldoc",
     "url" => "assets/images/project/logo_citaldoc_color.jpg"
     ],
     [
-    "logo" => "logo_rapi_color",
+    "logo" => "logo_rapi",
     "title" => "rapi",
     "url" => "assets/images/project/logo_rapi_color.jpg"
     ],
     [
-    "logo" => "logo_nidera_color",
+    "logo" => "logo_nidera",
     "title" => "nidera",
     "url" => "assets/images/project/logo_nidera_color.png"
     ],
