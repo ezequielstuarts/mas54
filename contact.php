cd0a6d207f2f920578bc4a03ee8442bf675eb606
Ezequiel <54673625+ezequielstuarts@users.noreply.github.com>
2020-04-20 11:21:52 -0300

    includes

--- contact.php
+++ contact.php
@@ -34,17 +34,8 @@
 
     <script src='https://www.google.com/recaptcha/api.js'></script>
 
-
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-29288138-38']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
+    <?php @include('includes/googlebar.php') ?>
+    
 </head>
 
 <body>
--- service.php
+++ service.php
@@ -33,16 +33,7 @@
     <!-- modernizr css -->
     <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
 
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-29288138-38']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>
+    <?php @include('includes/googlebar.php') ?>>
 
     
 </head>
